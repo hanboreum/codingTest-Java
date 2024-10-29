26cd5f0b699bf11b3fdde424e705eac8cd938a6a
fullm <fullmoon0207@naver.com>
2024-10-29 18:46:52 +0900

    feat: 백준 1000

---
+++ src/ch1/Boj1000.java
@@ -0,0 +1,12 @@
+package ch1;
+
+import java.util.Scanner;
+
+public class Boj1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A + B);
+    }
+}
