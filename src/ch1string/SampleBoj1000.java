7c752105427c94afcff6f069458c60ed859c9b03
fullm <fullmoon0207@naver.com>
2024-10-30 00:07:44 +0900

    feat: 백준 1000

--- src/ch1string/SampleBoj1000.java
+++ src/ch1string/SampleBoj1000.java
@@ -1,8 +1,8 @@
-package ch1;
+package ch1string;
 
 import java.util.Scanner;
 
-public class Boj1000 {
+public class SampleBoj1000 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
