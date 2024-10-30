0104e83a177f19f0cdbf86c4ee5c0b52b427e861
fullm <fullmoon0207@naver.com>
2024-10-31 01:56:16 +0900

    feat: 백준 1157 가장 많이 사용된 알파벳 대문자로 출력. 여러개 존재시 ? 출력

---
+++ src/ch1string/Boj1157.java
@@ -0,0 +1,29 @@
+package ch1string;
+
+import java.util.Scanner;
+
+//단어공부
+public class Boj1157 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'A']++;
+        }
+
+        int maxCount = 0;
+        char maxChar = '?';
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                maxChar = (char) ('A' + i);
+            } else if (count[i] == maxCount) {
+                maxChar = '?';
+            }
+        }
+        System.out.println(maxChar);
+    }
+}
