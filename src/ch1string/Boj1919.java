74f529b30293185f0f68ad170c5ffa3fce8de011
fullm <fullmoon0207@naver.com>
2024-10-31 01:24:11 +0900

    feat: 백준 1919

---
+++ src/ch1string/Boj1919.java
@@ -0,0 +1,29 @@
+package ch1string;
+
+import java.util.Scanner;
+
+//애너그램 만들기
+public class Boj1919 {
+
+    public static int[] getStringArray(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - 'a']++;
+        return count;
+    }
+
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        int[] countA = getStringArray(a);
+        int[] countB = getStringArray(b);
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+            ans += Math.abs(countA[i] - countB[i]);
+        System.out.println(ans);
+    }
+}
